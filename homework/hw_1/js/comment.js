c6b0fc001a95f54842405f020354fb6b3e62f69a
Antonina_Artukhova <antonina1992@gmail.com>
2013-05-28 19:15:12 +0400

    fix comment-form

--- homework/hw_1/js/comment.js
+++ homework/hw_1/js/comment.js
@@ -1,9 +1,13 @@
 function addCom() {
+
 		var days = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
 	var months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
 		var date = new Date();
-		$user = $('.user').val();
-		$comment = $('.tcomment').val();
+		$(".warn").hide();
+		$user = $('#user').val();
+		$comment = $('#tc').val();
+		$user1 = $('#user_answerer').val();
+		$comment1 = $('#tc_answerer').val();
 		$day = days[date.getDay()];
 		$d = date.getDate();
 		$month = months[date.getMonth()];
@@ -15,33 +19,41 @@ function addCom() {
 		if ($d < 10) {$d = '0'+$d;};
 		//if (month <10) {month = "0" + month;}*/
 		//flag=false;
-		if (($user!='')&&($comment!=''))
+		if ((($user!='')&&($comment!=''))||(($user1!='')&&($comment1!='')))
 		 {
-		 	$(".warn").hide();	
 		 	
+		 	if (($user1!='')&&($comment1!=''))
+		 	 { 
+		 	 	$user=$user1; 
+		 	 	$comment=$comment1; 
+		 	 	$('#ans_answerer').css("display","none");
+		 	 };
 		 	$com=' '+$hour+':'+$min+', '+$day+', '+$d+'.'+$month+'.'+$year;
 		 	$("#new_com").after("<p style='color:gray; '><strong style='color:black'>"+$user+"&nbsp;</strong>"+$com+"</p><p style='border-bottom: 1px solid  gray'>"+$comment+"</br></br><a href='#' >Ответить</a></p>");
 		 	
 		 	$(".form_el").find('input:text').val('').html();
 		 	$(".form_el").find('textarea').val('').html();
-		 	
+		 	$(".warn").hide();	
 		 } 
 			else{
 				$(".warn").show();
-				//$('.comment').after($('.warn'));
+				//$('.comment-form').after($('#warn'));
 		};
 			};
 function addAns () {
 	//$('#text_com').text(comment);
 	alert('test111');
-	$(this).after($('#ans_answerer'));	
+	$(".warn").hide();
+	$('#ans_answerer').css("display","block");
+	$(this).after($('#ans_answerer'));
+	$('#sub_answerer').on('click', addCom);	
 	};
 jQuery(document).ready(function() {
 	alert('test');
 	$(".warn").hide();
 	
 		$('#sub').on('click', addCom);
-		$('#sub_answerer').on('click', addCom);
+		
 	
 	$('.comment a').on('click', addAns);
 	});
